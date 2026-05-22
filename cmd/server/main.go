61d36a3b20090af83f34b5c5f3354970dee571f1
João Paulo Dias <joaopdias.dev@gmail.com>
2026-05-22 14:47:00 -0300

    Implement MongoDB client connection and update main server logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/Joaopdiasventura/life-compass-server/internal/database"
 )
 
 func main() {
+	uri := os.Getenv("MONGODB_URI")
+
+	database.NewClient(uri)
+
 	mux := http.NewServeMux()
 
 	server := &http.Server{
@@ -13,9 +20,9 @@ func main() {
 		Handler: mux,
 	}
 
-    log.Println("Server is running on :8080")
+	log.Println("Server is running on :8080")
 
-    if err := server.ListenAndServe(); err != nil {
-        log.Fatalf("Server failed: %v", err)
-    }
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
---
+++ internal/database/client.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func NewClient(uri string) *mongo.Client {
+
+	if uri == "" {
+		log.Fatal("Set the 'MONGODB_URI' environment variable")
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	return client
+}
